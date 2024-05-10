a6c199e275243fbbab99757b3c94e8c45ef24952
gittercoder <ryan2020in2020@gmail.com>
2024-05-11 01:28:56 +0530

    files-ordered
    files in proper directories

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import Home from "./components/home";
-import Login from "./components/login";
-import Signup from "./components/signup";
-import Tbuy from "./components/tbuy";
+import Home from "./components/Home/home";
+import Login from "./components/Login/login";
+import Signup from "./components/Signup/signup";
+import Tbuy from "./components/Tbuy/tbuy";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
